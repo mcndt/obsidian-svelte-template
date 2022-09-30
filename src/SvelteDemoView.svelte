8c997372c621ca4c4be4bf3757e9f333e676653c
Maxime Cannoodt <maxime@qargo.io>
2022-09-30 22:09:50 +0200

    basic setup

---
+++ src/SvelteDemoView.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		console.log("Hello from Svelte!");
+	});
+</script>
+
+<h3>Svelte Demo View</h3>
+<p>This is a demo view that uses Svelte.</p>
+<hr />
+
+<div class="button-div">
+	<button>Add item</button>
+	<button>Remove item</button>
+</div>
+
+<style>
+	.button-div {
+		display: flex;
+		column-gap: 8px;
+	}
+</style>
--- src/SvelteDemoView.ts
+++ src/SvelteDemoView.ts
@@ -1,9 +1,9 @@
-import { ButtonComponent, ItemView, WorkspaceLeaf } from "obsidian";
-
+import { ItemView, WorkspaceLeaf } from "obsidian";
+import SvelteDemoViewComponent from "./SvelteDemoView.svelte";
 export class SvelteDemoView extends ItemView {
 	static viewType = "SVELTE_DEMO_VIEW";
 
-	private data = [];
+	private component: SvelteDemoViewComponent;
 
 	constructor(leaf: WorkspaceLeaf) {
 		super(leaf);
@@ -12,30 +12,9 @@ export class SvelteDemoView extends ItemView {
 	/* Obsidian event lifecycle */
 
 	async onOpen(): Promise<void> {
-		this.renderView();
-	}
-
-	private renderView() {
-		this.contentEl.createEl("h3", { text: "Svelte Demo View" });
-		this.contentEl.createEl("p", {
-			text: "This is a demo view that uses the native Obsidian API.",
+		this.component = new SvelteDemoViewComponent({
+			target: this.contentEl,
 		});
-
-		this.contentEl.createEl("hr");
-
-		// Create buttons
-		const buttonDiv = this.contentEl.createDiv({ cls: "button-div" });
-		new ButtonComponent(buttonDiv).setButtonText("Add item");
-		new ButtonComponent(buttonDiv).setButtonText("Remove item");
-		buttonDiv.style.display = "flex";
-		buttonDiv.style.flexDirection = "row";
-		buttonDiv.style.columnGap = "5px";
-
-		// Create container for item list
-		const listDiv = this.contentEl.createDiv();
-		listDiv.style.display = "flex";
-		listDiv.style.flexDirection = "column";
-		listDiv.style.rowGap = "5px";
 	}
 
 	/* View abstract method implementations */
