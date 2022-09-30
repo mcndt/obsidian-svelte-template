bc194b25b0e0c55e25a080c9b58ad0656bc4c34a
Maxime Cannoodt <maxime@qargo.io>
2022-09-30 22:49:39 +0200

    reactive lists

--- src/SvelteDemoView.svelte
+++ src/SvelteDemoView.svelte
@@ -3,13 +3,21 @@
 	import { onMount } from "svelte";
 
 	let count = 0;
+	let data: { text: string }[] = [{ text: "hello!" }];
 
 	onMount(() => {
 		console.log("Hello from Svelte!");
 	});
 
-	function increment() {
-		count = count + 1;
+	function increment() {}
+
+	function onAdd() {
+		data = [...data, { text: "new item" }];
+	}
+
+	function onRemove() {
+		data.pop();
+		data = [...data];
 	}
 </script>
 
@@ -19,5 +27,35 @@
 
 <button on:click={increment}>Clicked {count} times</button>
 
+<hr />
+
+<div class="button-div">
+	<button on:click={onAdd}>Add item</button>
+	<button on:click={onRemove}>Remove item</button>
+	<span class="count">{data.length} items</span>
+</div>
+
+<ul class="list">
+	{#each data as item}
+		<li class="item">{item.text}</li>
+	{/each}
+</ul>
+
 <style>
+	.button-div {
+		display: flex;
+		flex-wrap: wrap;
+		column-gap: 8px;
+		row-gap: 8px;
+		/* flex center all items on horizontal axis */
+		align-items: center;
+	}
+
+	.count {
+		font-style: italic;
+	}
+
+	.list {
+		margin-top: 16px;
+	}
 </style>
